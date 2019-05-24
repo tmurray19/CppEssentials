b857b0a2334f4b62b204bf22a0d5f93a70ba242a
tmurray19 <me@taidghmurray.ie>
2019-05-24 14:56:30 +0100

    Discussing statements in C++

---
+++ Chapters/CppLearning/Statements.cpp
@@ -0,0 +1,21 @@
+// Statements
+
+#include <cstdio>
+#include <string>
+using namespace std;
+
+int main() {
+
+	int x;	// Statement
+	x = 42; // Expression
+	puts("Hello, World!");
+	// %d is a placeholder
+	// Statement, with expression x
+	printf("x is %d\n", x);
+	return 0;
+
+
+	//
+	string _private_identifier = "This has one underscore, indicating it is private";
+	string __system_use_only = "This has two underscores at the start, indicating it for system use only.";
+}
