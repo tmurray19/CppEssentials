f0badc7254560421a64a242d371a18c879679ece
tmurray19 <me@taidghmurray.ie>
2019-05-24 22:07:50 +0100

    Fixing ambiguous null pointers.

---
+++ Chapters/DataTypes/NullPointerConstant.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+using namespace std;
+
+#ifndef NULL
+#define NULL (0LL) //Common C++ definiton of NULL
+#endif
+
+void f(int i) {
+	printf("The int is %d.\n", i);
+}
+
+
+// Here f is overloaded
+void f(const char* s) {
+	printf("The pointer is %p.\n", s);
+}
+
+// If we call f with an int, the top f is called
+// If we call f with a string, the bottom f is called
+// nullptr is a null pointer of any type, pointing to a 0 in memory
+// Run the code to see details
+int main() {
+	f(3);
+	f("dsad");
+	f(NULL);
+	// Resolving ambiguous null pointer
+	f(nullptr);
+	return 0;
+}
