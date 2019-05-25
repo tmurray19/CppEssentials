e256dce2ff320a4ce3bd0c8b829b5cdc2a62a802
tmurray19 <me@taidghmurray.ie>
2019-05-25 15:34:34 +0100

    Directory restructure.

---
+++ Chapters/Operators/CommonOperators.cpp
@@ -0,0 +1,14 @@
+// Common Operators in C++
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+	// Assignment operator
+	int x = 5;
+	int y = 54;
+	printf("x is %d, y is %d.\n", x, y);
+
+	return 0;
+}
